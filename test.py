2973221f4f00488621dc0f9a11963e58af914b26
bestFried <64512591+bestFried@users.noreply.github.com>
2023-04-12 17:09:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests # requests 요청해서 텍스트 갖고오는 라이브러리
-from bs4 import BeautifulSoup # BeautifulSoup 갖고온 텍스트를 깔끔하게 가공해주는 라이브러리
+from bs4 import BeautifulSoup  # BeautifulSoup 갖고온 텍스트를 깔끔하게 가공해주는 라이브러리
 
-url = 'https://ridibooks.com/category/new-realeases/2200'
+url = 'https://ridibooks.com/category/new-releases/2200'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
-soup = BeautifulSoup(html, 'html.parser') #html파서, xml파서 등도 있음.
+soup = BeautifulSoup(html, 'html.parser') #xml파서 등도 있음
 
-bookservices = soup.select('.title_text') #특정 클래스 텍스트 짚어내는 코드
-for no, book in enumerate(bookservices, 1): #순회돌면서 넘버링해서 출력해주겠다
-  print(no, book.text.strip()) #strip은 trim
+bookservices = soup.select('.title_text')
+for no, book in enumerate(bookservices, 1):  #순회돌면서 넘버링해서 출력
+    print(no, book.text.strip()) #strip은 trim
